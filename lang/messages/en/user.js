0cc463d94de5accee4832d6aea772094ddef7fe0
namanhle <nle48@my.bcit.ca>
2025-09-11 03:52:25 -0700

    improved comment

--- lang/messages/en/user.js
+++ lang/messages/en/user.js
@@ -1,10 +1,10 @@
 const MESSAGES = {
-    INPUT_LABEL: "How many buttons to create?",
-    EXCELLENT_MEMORY: "Excellent memory!",
-    WRONG_ORDER: "Wrong order!",
-    INVALID_INPUT: "Please enter a number between 3 and 7",
-    MEMORIZE_POSITIONS: "Memorize the button positions!",
-    SCRAMBLING: "Scrambling buttons...",
-    CLICK_IN_ORDER: "Now click the buttons in their original order!",
-    GAME_STARTING: "Game starting..."
+  INPUT_LABEL: "How many buttons to create?",
+  EXCELLENT_MEMORY: "Excellent memory!",
+  WRONG_ORDER: "Wrong order!",
+  INVALID_INPUT: "Please enter a number between 3 and 7",
+  MEMORIZE_POSITIONS: "Memorize the button positions!",
+  SCRAMBLING: "Scrambling buttons...",
+  CLICK_IN_ORDER: "Now click the buttons in their original order!",
+  GAME_STARTING: "Game starting...",
 };
